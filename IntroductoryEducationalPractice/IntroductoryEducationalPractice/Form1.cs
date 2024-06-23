002f0599a07a88a2163a1305715c75bea1a64769
vasmaae <gutorov.ia1@gmail.com>
2024-06-23 23:06:27 +0400

    Create project

---
+++ IntroductoryEducationalPractice/IntroductoryEducationalPractice/Form1.cs
@@ -0,0 +1,10 @@
+namespace IntroductoryEducationalPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
